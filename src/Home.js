e9ca4627050a4df066f451a4faaa567e817c749e
djgrubac <grubacdjordje@gmail.com>
2023-08-11 14:23:09 +0200

    Testing

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='container'>
+            <div className='weather'>
+                <div className='search'>
+                    <input type='text' placeholder='Enter City name' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
